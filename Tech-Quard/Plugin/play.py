2931f283f45646d24bceb6de51b6290a67908a8e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <107318123+Sumit9969@users.noreply.github.com>
2022-06-15 17:12:00 +0530

    Update play.py

--- Tech-Quard/Plugin/play.py
+++ Tech-Quard/Plugin/play.py
@@ -15,7 +15,7 @@ from pytgcalls.types.input_stream.quality import (HighQualityAudio, HighQualityV
                                                   LowQualityVideo, MediumQualityVideo)
 from youtubesearchpython import VideosSearch
 
-from ArrayCore.Plguin.queue import QUEUE, add_to_queue, get_queue
+from Tech-Quard.Plguin.queue import QUEUE, add_to_queue, get_queue
 from . import CHAT_TITLE, gen_thumb
 from .. import vcbot, call_py, HNDLR, SUDO_USERS
 
@@ -63,7 +63,7 @@ async def ping(_, e: Message):
     if replied:
         if replied.audio or replied.voice:
             await e.delete()
-            TheVenomXD = await replied.reply_text("**Transcoding Mp3...**")
+            sumit = await replied.reply_text("**Transcoding Mp3...**")
             dl = await replied.download()
             link = replied.link
             if replied.audio:
@@ -75,23 +75,23 @@ async def ping(_, e: Message):
                 songname = "Voice Note"
             if chat_id in QUEUE:
                 pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-                await TheVenomXD.delete()
+                await sumit.delete()
                 await e.reply_text(f"**> Playing in:** {e.chat.title} \n\n**> Song:** {songname} \n**> Position:** #{pos}")
             else:
                 await call_py.join_group_call(chat_id, AudioPiped(dl), stream_type=StreamType().pulse_stream)
                 add_to_queue(chat_id, songname, dl, link, "Audio", 0)
-                await TheVenomXD.delete()
+                await sumit.delete()
                 await e.reply_text(f"**> Playing in:** {e.chat.title} \n\n**> Song:** {songname} \n**> Position:** Currently Playing")
     else:
         if len(e.command) < 2:
             await e.reply_text("Reply to Audio File or provide something for Searching ...")
         else:
             await e.delete()
-            TheVenomXD = await e.reply_text("__Searching...__")
+            sumit = await e.reply_text("__Searching...__")
             query = e.text.split(" ", 1)[1]
             search = ytsearch(query)
             if search == 0:
-                await TheVenomXD.edit("`Didn't Find Anything for the Given Query`")
+                await sumit.edit("`Didn't Find Anything for the Given Query`")
             else:
                 songname = search[0]
                 title = search[0]
@@ -105,18 +105,18 @@ async def ping(_, e: Message):
                 thumb = await gen_thumb(thumbnail, title, userid, ctitle)
                 hm, ytlink = await ytdl(url)
                 if hm == 0:
-                    await TheVenomXD.edit(f"**YTDL ERROR ️!** \n\n`{ytlink}`")
+                    await sumit.edit(f"**YTDL ERROR ️!** \n\n`{ytlink}`")
                 else:
                     if chat_id in QUEUE:
                         pos = add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
-                        await TheVenomXD.delete()
+                        await sumit.delete()
                         await e.reply_photo(photo=thumb, caption=f"**> Playing in:** {srrf} \n\n**> Song:** {songname} \n**> Position:** #{pos} \n\n**BY:** [{user_name}]({userid})")
                     else:
                         try:
                             await call_py.join_group_call(chat_id, AudioPiped(ytlink), stream_type=StreamType().pulse_stream)
                             add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
-                            await TheVenomXD.delete()
+                            await sumit.delete()
                             await e.reply_photo(photo=thumb, caption=f"**> Playing in:** {srrf} \n\n**> Song:** {songname} \n**> Position:** Currently Playing \n\n**By:** [{user_name}]({userid})")
                         except Exception as ep:
-                            await TheVenomXD.edit(f"`{ep}`")
+                            await sumit.edit(f"`{ep}`")
 
